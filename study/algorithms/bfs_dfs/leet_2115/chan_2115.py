77f50fdb0bd604e0fe7e8f4e15420b90f5048647
chan <seulchan07@gmail.com>
2023-02-13 10:08:19 +0900

    update leet 2115 comment

--- study/algorithms/bfs_dfs/leet_2115/chan_2115.py
+++ study/algorithms/bfs_dfs/leet_2115/chan_2115.py
@@ -4,9 +4,10 @@ from typing import List
 
 class Solution:
     def findAllRecipes(self, recipes: List[str], ingredients: List[List[str]], supplies: List[str]) -> List[str]:
-        graph = defaultdict(list)
-        indegree = defaultdict(int) # 
+        graph = defaultdict(list) # key: ingredient, value: recipe
+        indegree = defaultdict(int) # key: recipe, value: ingredient count
 
+        # graph, indegree 초기화
         for recipe, ingredient in zip(recipes, ingredients):
             indegree[recipe] = len(ingredient)
 
@@ -18,12 +19,15 @@ class Solution:
         queue = deque(supplies)
         recipes = set(recipes)
 
+
         while queue:
             supply = queue.popleft()
 
+            # supply가 recipe에 포함되어 있으면 ans에 추가
             if supply in recipes:
                 ans.append(supply)
             
+            # supply가 ingredient에 포함되어 있으면 recipe indegree 감소
             for recipie in graph[supply]:
                 indegree[recipie] -= 1
 
