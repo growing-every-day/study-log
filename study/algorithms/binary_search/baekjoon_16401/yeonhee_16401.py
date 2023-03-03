18dcb7eeeeeaabf1e16e05bd8bb0ad1bd64c20d8
YeonHee <lyh951212@naver.com>
2023-03-03 12:56:52 +0900

    이진탐색 코드

---
+++ study/algorithms/binary_search/baekjoon_16401/yeonhee_16401.py
@@ -0,0 +1,20 @@
+# https://www.acmicpc.net/problem/16401
+#  140328kb	3972ms
+import sys
+input = sys.stdin.readline
+m, n = map(int, input().split())
+L = list(map(int, input().split()))
+start = 1
+end = int(1e9)
+result = 0
+while start <= end:
+    mid = (start + end) // 2
+    countOfSnack = 0
+    for l in L:
+        countOfSnack += l // mid
+    if countOfSnack >= m:
+        result = max(result, mid)
+        start = mid + 1
+    else:
+        end = mid - 1
+print(result)
